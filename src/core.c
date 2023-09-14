e64d2638cb98a87c6be4286a9ab1022c75400c75
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2023-09-14 15:52:22 +0800

    Initial Commits

---
+++ src/core.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include "ctensive.h"
+
+int main(void)
+{
+    fprintf(stdout, "Hello World!");
+    return 0;
+}
---
+++ src/ctensive.h
@@ -0,0 +1,6 @@
+#ifndef CTENSIVE_H
+#define CTENSIVE_H
+
+// TODO: Commit & start coding
+
+#endif // CTENSIVE_H
